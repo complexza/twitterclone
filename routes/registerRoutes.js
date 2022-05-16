174e7494837b74c216925fcbfc7f4bfff2f9d909
Rian Lotter <rianlotter@icloud.com>
2022-05-16 18:57:10 +0200

    masterv2

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const bodyParser = require("body-parser")
 
 app.set("view engine", "pug");
 app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 router.get("/", (req, res, next) => {
 
@@ -12,7 +15,22 @@ router.get("/", (req, res, next) => {
 
 router.post("/", (req, res, next) => {
 
-  res.status(200).render("register");
+  var firstName = req.body.firstName.trim();
+  var lastName = req.body.lastName.trim();
+  var username = req.body.username.trim();
+  var email = req.body.email.trim();
+  var password = req.body.password;
+
+  var payload = req.body;
+
+  if(firstName && lastName && username && email && password) {
+
+  }
+  else {
+    payload.errorMessage = "Make sure each field is valid!"
+    res.status(200).render("register", payload);
+  }
+
 })
 
 module.exports = router;
